c58e7dbb17d61fb3579b72c21742717c9496ed25
stephen51 <istephenisac@gmail.com>
2022-09-11 10:39:12 +0530

    first commit

---
+++ src/main/java/com/stephen/jenkinsdockerkubernetesgke/JenkinsDockerKubernetesGkeApplication.java
@@ -0,0 +1,13 @@
+package com.stephen.jenkinsdockerkubernetesgke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDockerKubernetesGkeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDockerKubernetesGkeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stephen/jenkinsdockerkubernetesgke/JenkinsDockerKubernetesGkeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stephen.jenkinsdockerkubernetesgke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDockerKubernetesGkeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
