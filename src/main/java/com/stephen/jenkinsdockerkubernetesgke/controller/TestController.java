02ece022d50a79520766626dbc4bcdb931a1c3a9
stephen51 <istephenisac@gmail.com>
2022-09-14 23:22:17 +0530

    sonar fix-removed unused imports

--- src/main/java/com/stephen/jenkinsdockerkubernetesgke/controller/TestController.java
+++ src/main/java/com/stephen/jenkinsdockerkubernetesgke/controller/TestController.java
@@ -3,8 +3,6 @@ package com.stephen.jenkinsdockerkubernetesgke.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
-
 @RestController
 public class TestController {
 
